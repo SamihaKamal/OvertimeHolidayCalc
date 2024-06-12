d35c01569a1239e03612e40538a9523b6c2f0f2c
SamihaKamal <samihakamal.7664@gmail.com>
2024-06-12 14:41:41 +0100

    Done more stuff

--- App.js
+++ App.js
@@ -5,24 +5,27 @@ import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from 'rea
 export default function App() {
   const [timeWorked, SetTimeWorked] = useState(0);
   const [holidayCounter, setHolidayCounter] = useState(0);
+  const daysOfWeek = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
 
   const setHolidays = () => {
-    if (holidayCounter != 0){ 
-      setHolidayCounter(holidayCounter + timeWorked);
-    }
-    else{
-      setHolidayCounter(timeWorked);
-    }
-    
+    setHolidayCounter(prev => timeWorked + prev);
   }
+
   return (
     <View style={styles.container}>
+      <View styles={styles.dateContainer}>
+        {daysOfWeek.map((a, index) => (
+          <TouchableOpacity key={index} style={styles.dateBox}>
+            <Text>{a[0]}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
       <Text style={styles.counter}>{holidayCounter}</Text>
       <Text style={styles.text}>Enter overtime hours: </Text>
       <TextInput 
       style={styles.textBox}
       value={timeWorked}
-      onChangeText={SetTimeWorked}
+      onChangeText={text => SetTimeWorked(Number(text))}
       keyboardType='numeric'/>
       <StatusBar style="auto" />
       <TouchableOpacity style={styles.button} onPress={setHolidays}>
@@ -37,7 +40,24 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
+  },
+
+  dateContainer: {
+    flexDirection: 'row',
     justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 20, // Added some vertical margin for spacing
+  },
+
+  dateBox: {
+    height: 50,
+    backgroundColor: "#FFCCBC",
+    margin: 5,
+    borderRadius: 10,
+    width: 50,
+    marginHorizontal: 10,
+    justifyContent: "center",
+    alignItems: "center",
   },
 
   text: {
